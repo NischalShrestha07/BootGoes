73adbedcd02675593aaecc8e090e574bc8379f5d
Nischal <nischalshrestha800@gmail.com>
2026-06-17 21:55:11 +0545

    feat(auth updates)

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login()
+    {
+        return view('layouts.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|min:5',
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return redirect()->route('dashboard.index');
+        }
+        return redirect()->route('login');
+    }
+
+    public function register()
+    {
+        return view('layouts.register');
+    }
+
+    public function addUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'string|required|min:5',
+            'email' => 'required|email',
+            'password' => 'required|string|min:5|confirmed',
+        ]);
+        // return $request;
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return redirect()->route('login');
+    }
+    public function forgotPassword()
+    {
+        return view('layouts.forgot-password');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [UserController::class, 'login'])->name('login');
+Route::post('/login', [UserController::class, 'authenticate'])->name('authenticate');
+Route::get('/register', [UserController::class, 'register'])->name('register');
+Route::post('user/register/', [UserController::class, 'addUser'])->name('register.user');
+
+Route::get('/forgot-password', [UserController::class, 'forgotPassword'])->name('forgot.password');
